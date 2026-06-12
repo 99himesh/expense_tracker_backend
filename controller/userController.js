d0507962dd79d48774ddf60b336c15d34bffc0c8
mac <mac@macs-MacBook-Pro.local>
2026-06-12 13:13:05 +0530

    jwt token

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,11 @@
 const Users = require("../models/UserModel");
 const bcrypt=require("bcrypt")
+const jwt=require("jsonwebtoken");
+
+const generatejwtToken=async(userId,name)=>{
+   return await jwt.sign({ userId: userId,name:name }, "jdfgshjfbhjdsbfhjsdbjhfbsdjhfsd"); 
+}
+
 const signUp=async(req,res)=>{
     try {
         const {name,email,password}=req.body;
@@ -39,12 +45,12 @@ const logIn=async(req,res)=>{
            return res.status(404).send("User not exist");
         }
        
-        bcrypt.compare(password,user[0]?.password,(err,result)=>{  
+        bcrypt.compare(password,user[0]?.password,async(err,result)=>{  
               if(err){
                throw new Error("Something went wrong!")
              }         
-            if(result){
-              res.status(200).json({user,message:"User Login successfully"})
+            if(result){  
+              res.status(200).json({user,token:await generatejwtToken(user[0].id,user[0].name),message:"User Login successfully"})
              }else{
              return res.status(401).send("User not authorized");
             }
