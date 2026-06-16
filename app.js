ac3927312292a6a7456161abda6b49050666d7c1
mac <mac@macs-MacBook-Pro.local>
2026-06-16 15:25:35 +0530

    update env

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ const accessLogStream=fs.createWriteStream(
     path.join(__dirname,"ccess.log"),
     {flags:'a'}
 )
-app.use(cors());
+app.use(cors({
+  origin: "http://localhost:5173"
+}));
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 //helmet is used when you host in domain 
