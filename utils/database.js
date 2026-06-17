9f3dbbbaee5b5bd628a9aabd67f76eb160f138ed
mac <mac@macs-MacBook-Pro.local>
2026-06-17 21:21:04 +0530

    update nodemon

--- utils/database.js
+++ utils/database.js
@@ -1,5 +1,5 @@
 const {Sequelize}=require('sequelize');
-
+  
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.USER_NAME, process.env.DB_PASSWORD, {
   host: process.env.DB_HOST,
   dialect: 'mysql'
