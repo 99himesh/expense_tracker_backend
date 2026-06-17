7580d5ee510077007e257cde97e20a86d4cedf67
mac <mac@macs-MacBook-Pro.local>
2026-06-17 20:34:30 +0530

    update nodemon

--- controller/aiController.js
+++ controller/aiController.js
@@ -4,6 +4,7 @@ console.log(process.env.GEMINI_API_KEY,"GEMINI_API_KEY");
 const ai = new GoogleGenAI({apiKey:process.env.GEMINI_API_KEY});
 const aiGpt=async(req,res)=>{
     const {question}=req.body;
+    //hii changes done
     try {
             const response = await ai.models.generateContent({
                 model: 'gemini-2.5-flash',
